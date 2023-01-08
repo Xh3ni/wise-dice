296031808db133bf29dc4a5dcd7578867b501aaa
Xh3ni <xhensilastambolliu@yahoo.com>
2023-01-08 17:14:22 +0000

    Add event listener to the button element after the page finishs loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button element and add event listener
+
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.getElementById("roll");
+    button.addEventListener("click", function() {
+        rollDice();
+
+    });
+});
+
 function rollDice(){}
 
 function incrementPlayerResult() {}
